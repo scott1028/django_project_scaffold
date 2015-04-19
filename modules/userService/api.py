d2484256513dcfb636b188e9b11da3e7350cdb90
scott <mic1028002@gmail.com>
2015-04-19 10:42:04 +0800

    update sample

--- modules/userService/api.py
+++ modules/userService/api.py
@@ -1,10 +1,15 @@
 # coding: utf-8
 # ref: http://www.django-rest-framework.org/#installation
+# ref: http://www.django-rest-framework.org/api-guide/filtering/#filtering-against-query-parameters
+# ref: http://www.django-rest-framework.org/api-guide/filtering/#setting-filter-backends
 
 
 from django.conf.urls import url, include
 from modules.daoService.models import User
 from rest_framework import routers, serializers, viewsets
+from rest_framework import filters
+from rest_framework.response import Response
+
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -16,7 +21,45 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+
+    # def list(self, request):
+    #     print self.request.QUERY_PARAMS
+    #     username = self.request.QUERY_PARAMS.get('username', None)
+    #     queryset = User.objects.all()
+    #     serializer = UserSerializer(queryset, many=True, context={'request': request})
+    #     return Response(serializer.data)
+
+from rest_framework import generics
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        username = self.kwargs.get('username', None) or self.request.QUERY_PARAMS.get('username', None)
+        filters = {}
+        if username is not None:
+            filters['username'] = username
+        return User.objects.filter(**filters)
+
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+
+# from rest_framework import generics
+
+
+# class UserList(generics.ListAPIView):
+#     serializer_class = UserSerializer
+
+#     def get_queryset(self):
+#         """
+#         This view should return a list of all the purchases
+#         for the currently authenticated user.
+#         """
+#         user = self.request.user
+#         return User.objects.filter(username='1')
+
+# router.register(r'users2', UserList)
--- modules/userService/urls.py
+++ modules/userService/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 from modules.userService.views import UserModelResource
 from modules.userService.api import router
+from modules.userService.api import UserListView
 
 urlpatterns = patterns('',
     url(r'^login/', 'modules.userService.views.makeUserLogin', name='login'),
@@ -13,4 +14,7 @@ urlpatterns = patterns('',
 
     # DjangoRestful Framework
     url(r'^v2/', include(router.urls)),
+
+    url(r'^v3/userList/(?P<username>.+)/$', UserListView.as_view()),
+    url(r'^v3/userList/$', UserListView.as_view()),
 )
